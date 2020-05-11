d5de5dc1429a99bb21f3cd90613007fb104a0a03
Ryan Harris <ragobash@gmail.com>
2020-05-11 10:45:55 -0500

    Updates

---
+++ precache-manifest.add8a7c712acc6a3c8d52db7ba8add0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d122abcb64c05ddd3171b4d3977b1a5e",
+    "url": "/spenga/index.html"
+  },
+  {
+    "revision": "bf99323baa74f324f527",
+    "url": "/spenga/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ce8d9b94a8b4a0b0bf21",
+    "url": "/spenga/static/js/2.ed0a020d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spenga/static/js/2.ed0a020d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf99323baa74f324f527",
+    "url": "/spenga/static/js/main.096014cb.chunk.js"
+  },
+  {
+    "revision": "f17da6cf4999a6f43a9a",
+    "url": "/spenga/static/js/runtime-main.d8a6612b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spenga/static/media/logo.5d5d9eef.svg"
+  }
+]);
